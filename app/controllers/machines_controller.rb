d33189fda5ab93ee55fd4d914d80a4a8b889a973
ManjaBadel <m.badel@gmx.de>
2021-04-29 20:29:54 +0200

    Creating a new machine

--- app/controllers/machines_controller.rb
+++ app/controllers/machines_controller.rb
@@ -7,4 +7,25 @@ class MachinesController < ApplicationController
   def show
     @machine = Machine.find(params[:id])
   end
+
+  def new
+    @machine = Machine.new
+  end
+
+  def create
+    @machine = Machine.new(machine_params)
+    @machine.user = current_user
+    if @machine.save
+      redirect_to machine_path(@machine)
+    else
+      render :new
+    end
+  end
+
+
+  private
+
+  def machine_params
+    params.require(:machine).permit(:location, :business_type, :business_name, :machine_type, :price, :availability, :user_id)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   # get '/machines', to: 'machines#index'
   # get '/machines/:id', to: 'machines#show'
-  resources :machines, only: [:index, :show]
+  resources :machines, only: [:index, :show, :new, :create]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
