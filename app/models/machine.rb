181ffdf03bfbec6a29a406c0581cb43ff34210cf
ManjaBadel <m.badel@gmx.de>
2021-05-04 01:32:27 +0200

    Address Autocomplete in new form

--- app/models/machine.rb
+++ app/models/machine.rb
@@ -5,7 +5,7 @@ class Machine < ApplicationRecord
   validates :business_name, length: { minimum: 3 }
   validates :business_type, inclusion: { in: ["Cafe","Bar", "Gym", "Shopping Mall", "Coworking Space", "University & Libraries", "Other"]}
   validates :machine_type, inclusion: { in: ["Washer", "Dryer", "Washer & Dryer"]}
-  validates :price, numericality: { greater_than: 0 }
   geocoded_by :location
   after_validation :geocode, if: :will_save_change_to_location?
+  validates :price, format: { with: /\A\d+(?:\.\d{0,2})?\z/ }, numericality: { greater_than: 0, less_than: 1000000 }
 end
