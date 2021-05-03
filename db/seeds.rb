a78a08e8acab75e282f954a7d18fbdeebde6e409
ManjaBadel <m.badel@gmx.de>
2021-05-03 21:40:30 +0200

    Building Mapbox

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,9 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-Machine.create(user_id: 1, machine_type: "washer", business_type: "Cafe", location: "Stargarder Str. 55a, 10437 Berlin", business_name: "Cafe Ole", price: 5.9)
-Machine.create(user_id: 2, machine_type: "washer", business_type: "Cafe", location: "Weinmeisterstraße 9a, 10178 Berlin", business_name: "KAFFEEMITTE", price: 6.9)
-Machine.create(user_id: 2, machine_type: "washer", business_type: "Cafe", location: "Weinbergsweg 20, 10119 Berlin", business_name: "Café Fleury", price: 6.9)
-Machine.create(user_id: 2, machine_type: "washer", business_type: "Cafe", location: "Bergstraße 68, 10115 Berlin", business_name: "Distrikt coffee", price: 6.9)
-Machine.create(user_id: 2, machine_type: "washer", business_type: "Cafe", location: "Schwarzwaldstraße 44, 60528 Frankfurt am Main", business_name: "Milch Halle Cafe", price: 6.9)
-Machine.create(user_id: 2, machine_type: "washer", business_type: "Cafe", location: "Leipziger Str. 108, 60487 Frankfurt am Main", business_name: "Fridas Cafe", price: 6.9)
+# Machine.create(user_id: 1, machine_type: "washer", business_type: "Cafe", location: "Stargarder Str. 55a, 10437 Berlin", business_name: "Cafe Ole", price: 5.9)
+# Machine.create(user_id: 2, machine_type: "washer", business_type: "Cafe", location: "Weinmeisterstraße 9a, 10178 Berlin", business_name: "KAFFEEMITTE", price: 6.9)
+# Machine.create(user_id: 2, machine_type: "washer", business_type: "Cafe", location: "Weinbergsweg 20, 10119 Berlin", business_name: "Café Fleury", price: 6.9)
+# Machine.create(user_id: 2, machine_type: "washer", business_type: "Cafe", location: "Bergstraße 68, 10115 Berlin", business_name: "Distrikt coffee", price: 6.9)
+# Machine.create(user_id: 2, machine_type: "washer", business_type: "Cafe", location: "Schwarzwaldstraße 44, 60528 Frankfurt am Main", business_name: "Milch Halle Cafe", price: 6.9)
+# Machine.create(user_id: 2, machine_type: "washer", business_type: "Cafe", location: "Leipziger Str. 108, 60487 Frankfurt am Main", business_name: "Fridas Cafe", price: 6.9)
+
+# 15.times do
+#   Maschine.create(user_)
+# end
