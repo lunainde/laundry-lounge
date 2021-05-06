b82c5c7c6ec9c366b18233b71e51f52793890be4
ManjaBadel <m.badel@gmx.de>
2021-05-06 19:34:44 +0200

    search functions

--- app/controllers/machines_controller.rb
+++ app/controllers/machines_controller.rb
@@ -1,18 +1,28 @@
 class MachinesController < ApplicationController
 
   def index
-    if params[:query].present?
-      @machines = Machine.where(business_type: params[:query])
-    else
-      @machines = Machine.all
-      @markers = @machines.geocoded.map do |machine|
+    # if params[:query].present?
+    #   @machines = Machine.where(business_type: params[:query])
+    # else
+    #   @machines = Machine.all
+    #   @markers = @machines.geocoded.map do |machine|
+    #   {
+    #     lat: machine.latitude,
+    #     lng: machine.longitude,
+    #     infoWindow: render_to_string(partial: "info_window", locals: { machine: machine }),
+    #     image_url: helpers.asset_url('map-solid.png')
+    #   }
+    #   end
+    # end
+    @machines = Machine.all
+    filter_machines if params[:search]
+    @markers = @machines.geocoded.map do |machine|
       {
         lat: machine.latitude,
         lng: machine.longitude,
         infoWindow: render_to_string(partial: "info_window", locals: { machine: machine }),
         image_url: helpers.asset_url('map-solid.png')
       }
-      end
     end
   end
 
@@ -59,4 +69,13 @@ class MachinesController < ApplicationController
     params.require(:machine).permit(:location, :business_type, :business_name, :machine_type, :price, :availability, :user_id)
   end
 
+  def filter_machines
+    search = params[:search]
+    # @machines = Machine.where('location ILIKE ?', "%#{search[:location]}%") unless search[:location].empty?
+    @machines = Machine.near("%#{search[:location]}%", 5) unless search[:location].empty?
+
+    # @machines = Machine.where('date ILIKE ?', "%#{search[:date]}%") unless search[:date].empty?
+    # @machines = Machine.where('time ILIKE ?', "%#{search[:time]}%") unless search[:time].empty?
+    @machines = Machine.where('business_type ILIKE ?', "%#{search[:business_type]}%") unless search[:business_type].empty?
+  end
 end
