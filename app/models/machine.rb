83882e11aead51757b23c87361d1018c6e6f34c8
Diana Ka <75699816+lunainde@users.noreply.github.com>
2021-05-08 15:48:43 +0200

    image seed fixed

--- app/models/machine.rb
+++ app/models/machine.rb
@@ -1,7 +1,7 @@
 class Machine < ApplicationRecord
   belongs_to :user
   has_many_attached :photos, dependent: :destroy
-  validates :photos, length: { minimum: 4 }
+
   has_many :bookings, dependent: :destroy
   validates :business_name, :business_type, :location, :machine_type, :price, presence: true
   validates :business_name, length: { minimum: 3 }
