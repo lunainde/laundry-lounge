d13bfb2cb4569fa7bc8f7a077c865111bc45afc6
Awa Said <awa.h.said@gmail.com>
2021-05-08 14:57:20 +0200

    image file seed

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_06_174324) do
+ActiveRecord::Schema.define(version: 2021_05_08_104924) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -67,6 +67,7 @@ ActiveRecord::Schema.define(version: 2021_05_06_174324) do
     t.float "latitude"
     t.float "longitude"
     t.text "description"
+    t.string "img"
     t.index ["user_id"], name: "index_machines_on_user_id"
   end
 
